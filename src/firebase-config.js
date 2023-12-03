361599cf8a91a7022c1070652d7f8cb96bab0426
Siiriellisylvia <113165765+Siiriellisylvia@users.noreply.github.com>
2023-12-03 16:31:35 +0100

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,3 +22,6 @@ export const db = getFirestore(firebaseApp);
 
 export const usersRef = collection(db, "users");
 export const recipesRef = collection(db, "recipes");
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(firebaseApp);
